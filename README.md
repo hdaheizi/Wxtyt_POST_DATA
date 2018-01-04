513705cec17335aff0394d305cb5b69102397277
Seayon <153660639@qq.com>
2018-01-04 22:19:54 +0800

    更新了ReadMe.MD

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Wxtyt_POST_DATA
 微信跳一跳小游戏抓包修改分数，Java实现
+#### Usage
+````
+String sessionid = "session_id";//session_id值
+String score = "100";//分数
+String result = Util.postData(score,  sessionid);//执行请求
+System.out.println(result);//输出分数
+````
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -11,15 +11,16 @@ import java.io.UnsupportedEncodingException;
  * @date: 2017/12/31
  * @time: 18:45
  */
+
 /**
-* JSON
-*/
+ * JSON
+ */
 public class Main {
     public static void main(String[] args) {
         String sessionid = "zS7m19dyi83wQIJmpDKX5qWcumYPUx1f8KNkxk1LG8e8CIcZ8emXs2cx0XFslY1q8k1WctCLXCXGUrdk4wgC3VuQf1qsvIUH0LzxnB2zAmLZH3I2Kt4gVSLOrMcTp8xwBFNNfl9fNXrd4gq8qBy73w==";
         String score = "100";
         String times = "700";
-        String result = Util.postData(score,  sessionid);
+        String result = Util.postData(score, sessionid);
         System.out.println(result);
     }
 }
